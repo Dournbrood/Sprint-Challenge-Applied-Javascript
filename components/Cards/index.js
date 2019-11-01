f92fd838f6f72e21c1f07a5795a8070a510ba3a6
Dournbrood <tanner1220002@gmail.com>
2019-11-01 12:07:17 -0400

    MVP completion

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then(response => {
+        console.log(response);
+
+        const cardContainer = document.querySelector(`.cards-container`);
+
+        const articleList = Object.keys(response.data.articles);
+
+        console.log(articleList);
+
+        articleList.forEach((element) => {
+            response.data.articles[element].forEach((subElement) => {
+                cardContainer.appendChild(createArticle(subElement));
+            });
+        });
+    });
+
+function createArticle(data) {
+    //Construct elements
+    const
+        card = document.createElement(`div`),
+        headline = document.createElement(`div`),
+        author = document.createElement(`div`),
+        imgContainer = document.createElement(`div`),
+        authorImg = document.createElement(`img`),
+        authorName = document.createElement(`span`);
+
+    //Set contents/styling
+    headline.textContent = data.headline;
+    authorImg.src = data.authorPhoto;
+    authorName.textContent = data.authorName;
+
+    //Set classes
+    card.classList.add(`card`);
+    headline.classList.add(`headline`);
+    author.classList.add(`author`);
+    imgContainer.classList.add(`img-container`);
+
+    //Define structure
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(authorImg);
+    author.appendChild(authorName);
+
+    //Add events    
+
+
+    return card;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+    .then(response => {
+        console.log(response);
+        const topicBar = document.querySelector(`.topics`);
+        response.data.topics.forEach((element) => {
+            topicBar.appendChild(buildTab(element));
+        });
+    });
+
+function buildTab(topic) {
+    //Construct elements
+    const
+        tab = document.createElement(`div`);
+
+    //Set contents/styling
+    tab.textContent = topic;
+
+    //Set classes
+    tab.classList.add(`tab`);
+
+    //Define structure
+
+
+    //Add events
+
+
+    return tab;
+}
